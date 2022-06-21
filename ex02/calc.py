f8d1fde5c929aabe44de1e958a48dd4b10662e77
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-06-21 14:49:53 +0900

    4

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,14 +6,17 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn=event.widget
     num=btn["text"]
-    tkm.showinfo("",f"{num}のボタンがクリックされました")
-
+    #tkm.showinfo("",f"{num}のボタンがクリックされました")
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("超高機能電卓")
     root.geometry("300x500")
 
-    r,c=0,0      #ｒ：行番号　ｃ：列番号
+    entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=3)
+
+
+    r,c=1,0      #ｒ：行番号　ｃ：列番号
     for i in range(9,-1,-1):
         btn=tk.Button(root,
                     text=i,
@@ -22,7 +25,6 @@ if __name__ == "__main__":
                     font=("Times New Roman",30)
                     )
         btn.bind("<1>",button_click)
-
         btn.grid(row=r,column=c)
         c+=1
         if (i-1)%3==0:
