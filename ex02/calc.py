d1408d31cc25ae6e249f24e211a74db532b7430c
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-06-21 14:58:03 +0900

    6

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,16 +11,17 @@ def button_click(event):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("超高機能電卓")
-    root.geometry("300x500")
+    #root.geometry("300x500")
 
     entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
     entry.grid(row=0,column=0,columnspan=3)
 
 
+
     r,c=1,0      #ｒ：行番号　ｃ：列番号
-    for i in range(9,-1,-1):
+    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]):
         btn=tk.Button(root,
-                    text=i,
+                    text=f"{num}",
                     width=4,
                     height=2,
                     font=("Times New Roman",30)
@@ -28,7 +29,7 @@ if __name__ == "__main__":
         btn.bind("<1>",button_click)
         btn.grid(row=r,column=c)
         c+=1
-        if (i-1)%3==0:
+        if (i+1)%3==0:
             r+=1
             c=0
     root.mainloop()
