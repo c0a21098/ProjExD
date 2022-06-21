711394c5cff20a3d7364ca80fc4e063a4c69b392
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-06-21 15:26:18 +0900

    7

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,24 +2,35 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
-
 def button_click(event):
     btn=event.widget
     num=btn["text"]
+    if num =="=":
+        eqn=entry.get()
+        ans=eval(eqn)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,ans)
     #tkm.showinfo("",f"{num}のボタンがクリックされました")
-    entry.insert(tk.END,num)
+    else:
+        entry.insert(tk.END,num)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("超高機能電卓")
     #root.geometry("300x500")
 
-    entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
+    entry = tk.Entry(root,
+                    justify="right",
+                    width=10,
+                    font=("Times New Roman",40)
+                    )
     entry.grid(row=0,column=0,columnspan=3)
 
 
 
     r,c=1,0      #ｒ：行番号　ｃ：列番号
-    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]):
+    for i,num in enumerate([i for i in range(9,-1,-1)]+["+","="]):
         btn=tk.Button(root,
                     text=f"{num}",
                     width=4,
