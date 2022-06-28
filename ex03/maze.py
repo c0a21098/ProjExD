43f8311669cda42576a966a859946190f53a4833
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-06-28 15:32:59 +0900

    8

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -12,14 +13,15 @@ def key_up(event):
     #print(f"{key}が離されました")
 
 def main_proc():
-    global cx, cy
+    global mx, my, cx, cy
     delta = {
         "":[0,0],
-        "Up":[0,-20],  #キー：押されたキー、値：移動幅リスト[x,y]
-        "Down":[0,20],
-        "Left":[-20,0],
-        "Right":[20,0]}
-    cx, cy = cx+delta[key][0], cy+delta[key][1]
+        "Up":[0,-1],  #キー：押されたキー、値：移動幅リスト[x,y]
+        "Down":[0,1],
+        "Left":[-1,0],
+        "Right":[1,0]}
+    mx, my = mx+delta[key][0], my+delta[key][1]
+    cx, cy = mx*100+50, my*100+50
     canvas.coords("tori", cx, cy)
     root.after(100,main_proc)
 
@@ -28,14 +30,20 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     
-    canvas = tk.Canvas(root, 
-                       width=1500, 
-                       height=900,
-                       bg="black")
+    canvas = tk.Canvas(
+                    root, 
+                    width=1500, 
+                    height=900,
+                    bg="black")
     canvas.pack()
+    maze_bg = mm.make_maze(15,9)
+    mm.show_maze(canvas,maze_bg)
+    #print(maze_bg)
+
 
     tori = tk.PhotoImage(file="fig/6.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
