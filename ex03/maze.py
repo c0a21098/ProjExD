727237c5ac70383bc8a5cbfc67d753f9b07f9840
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-06-28 14:38:22 +0900

    5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+def key_down(event):
+    global key
+    key = event.keysym
+    print(f"{key}が押されました")
+
+
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -16,4 +24,6 @@ if __name__ == "__main__":
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
+
+    root.bind("<KeyPress>",key_down)
     root.mainloop()
