5d3c7d884b6285dd36e84e9687b875fd6454e549
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-07-05 14:23:19 +0900

    3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,18 +2,26 @@ import pygame as pg
 import sys
 
 def main():
+    clock = pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600,900))  #スクリーンのSurface
     screen_rct = screen_sfc.get_rect()            #スクリーンのRect
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")    #Surface
     bgimg_rct = bgimg_sfc.get_rect()              #Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
+    kkimg_sfc = pg.image.load("fig/9.png")        #こうかとんのSurface
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
+    kkimg_rct = kkimg_sfc.get_rect()             #こうかとんのRect
+    kkimg_rct.center =900, 400                   #こうかとんの表示位置
 
     while True:
-        screen_sfc.blit(bgimg_sfc,bgimg_rct)
-
+        screen_sfc.blit(bgimg_sfc,bgimg_rct)      
+        screen_sfc.blit(kkimg_sfc,kkimg_rct)
         for event in pg.event.get():
-            if event.type == pg.QUIT:return
+            if event.type == pg.QUIT:return       #×ボタンが押されたら関数から抜け出す
+        
+        pg.display.update()
+        clock.tick(1000)
 
 
 
