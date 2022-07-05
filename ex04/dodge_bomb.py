ca29c05fa3ae57b6b51c5536cebe773f159943df
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-07-05 15:29:04 +0900

    7

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -42,6 +42,15 @@ def main():
             kkimg_rct.centerx -=1
         if key_states[pg.K_RIGHT] == True:
             kkimg_rct.centerx +=1
+        if check_bound(kkimg_rct, screen_rct) !=(1, 1):
+            if key_states[pg.K_UP] == True:         
+                kkimg_rct.centery +=1
+            if key_states[pg.K_DOWN] == True:
+                kkimg_rct.centery -=1
+            if key_states[pg.K_LEFT] == True:
+                kkimg_rct.centerx +=1
+            if key_states[pg.K_RIGHT] == True:
+                kkimg_rct.centerx -=1
 
         bmimg_rct.move_ip(vx, vy)
 
@@ -49,11 +58,25 @@ def main():
 
         screen_sfc.blit(bmimg_sfc,bmimg_rct)
 
+        yoko, tate = check_bound(bmimg_rct, screen_rct)
+        vx*=yoko
+        vy*=tate
 
         pg.display.update()
         clock.tick(1000)
 
 
+def check_bound(rct,scr_rct):
+    '''
+    [1]rct:こうかとん or 爆弾のRect
+    [2]scr_rct:スクリーンのRect
+    '''
+    yoko, tate = +1, +1  #領域内
+    if rct.left < scr_rct.left or scr_rct.right < rct.right: 
+        yoko = -1        #領域外
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom:
+        tate = -1
+    return yoko, tate
 
 
 
