c67d5c443f6fe2fc0215bfb570c83001506d3912
c0a21098 <c0a21098e5@edu.teu.ac.jp>
2022-07-05 14:53:59 +0900

    5,6

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()
@@ -9,10 +10,20 @@ def main():
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")    #Surface
     bgimg_rct = bgimg_sfc.get_rect()              #Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
+
+
     kkimg_sfc = pg.image.load("fig/9.png")        #こうかとんのSurface
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
     kkimg_rct = kkimg_sfc.get_rect()             #こうかとんのRect
     kkimg_rct.center =900, 400                   #こうかとんの表示位置
+
+    bmimg_sfc = pg.Surface((20, 20))                #Surface
+    bmimg_sfc.set_colorkey((0,0,0))                 #爆弾の周りの黒を消す
+    pg.draw.circle(bmimg_sfc, (255,0,0),(10,10),10)  
+    bmimg_rct = bmimg_sfc.get_rect()                 #Rect
+    bmimg_rct.centerx = random.randint(0,screen_rct.width)
+    bmimg_rct.centery = random.randint(0,screen_rct.height)
+    vx, vy = +1, +1
 
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)      
@@ -31,7 +42,14 @@ def main():
             kkimg_rct.centerx -=1
         if key_states[pg.K_RIGHT] == True:
             kkimg_rct.centerx +=1
+
+        bmimg_rct.move_ip(vx, vy)
+
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
+
+        screen_sfc.blit(bmimg_sfc,bmimg_rct)
+
+
         pg.display.update()
         clock.tick(1000)
 
